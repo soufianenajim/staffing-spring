0ea508f003242995c5fb8420631072253cb05ca0
soufiane-najim <soufianenajime@gmail.com>
2018-07-26 14:05:18 +0100

    first commit

---
+++ src/main/java/com/staffing/app/StaffingSpringApplication.java
@@ -0,0 +1,12 @@
+package com.staffing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffingSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/staffing/app/StaffingSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.staffing.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StaffingSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
