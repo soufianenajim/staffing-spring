20b00eab37f01063ba57d8f07ec30d430f911bbd
hzouitine <3zouitine@gmail.com>
2018-07-27 15:24:42 +0100

    Add data

---
+++ src/main/java/com/staffing/app/utils/AddData.java
@@ -0,0 +1,56 @@
+package com.staffing.app.utils;
+
+import java.util.Date;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import com.staffing.app.dao.EmployeeProjectRepository;
+import com.staffing.app.dao.EmployeeRepository;
+import com.staffing.app.dao.ProjectRepository;
+import com.staffing.app.model.Employee;
+import com.staffing.app.model.EmployeeProject;
+import com.staffing.app.model.Project;
+
+@Component
+public class AddData implements ApplicationListener<ContextRefreshedEvent> {
+
+	EmployeeRepository employeeRepository;
+	EmployeeProjectRepository employeeProjectRepository;
+	ProjectRepository projectRepository;
+
+	public AddData(EmployeeRepository employeeRepository, EmployeeProjectRepository employeeProjectRepository,
+			ProjectRepository projectRepository) {
+		this.employeeRepository = employeeRepository;
+		this.employeeProjectRepository = employeeProjectRepository;
+		this.projectRepository = projectRepository;
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		// TODO Auto-generated method stub
+		save();
+
+	}
+
+	public void save() {
+		Employee emp = new Employee();
+		emp.setName("Hamza");
+		Employee empSaved = employeeRepository.save(emp);
+		
+		
+		Project p = new Project();
+		p.setName("Victoria");
+		Project pSaved = projectRepository.save(p);
+		
+		EmployeeProject empr = new EmployeeProject();
+		empr.setRefEmployee(empSaved);
+		empr.setRefProject(pSaved);
+		empr.setDateFrom(new Date());
+		empr.setDateTo(new Date());
+		employeeProjectRepository.save(empr);
+
+	}
+
+}
