1fdf7a302b2947f59d29f403e1aa9f87f503847e
hzouitine <3zouitine@gmail.com>
2018-07-31 16:29:34 +0100

    enable cross origin

--- src/main/java/com/staffing/app/rest/MainController.java
+++ src/main/java/com/staffing/app/rest/MainController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +26,6 @@ public class MainController {
 
 		System.out.println("from "+fromDate+" to "+toDate);
 		return employeeService.findAllBetween(fromDate, toDate);
-	}
-
-	@RequestMapping("/findAll")
-	public List<EmployeeDTO> findAll() {
-		return employeeService.findAllBetween(new Date(), new Date());
 	}
 
 }
