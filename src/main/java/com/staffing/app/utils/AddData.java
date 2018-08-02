3a3634fd825be03bb57a6b52741185758981f4ff
hzouitine <3zouitine@gmail.com>
2018-08-02 09:23:54 +0100

    Add staffing mapping

--- src/main/java/com/staffing/app/utils/AddData.java
+++ src/main/java/com/staffing/app/utils/AddData.java
@@ -1,7 +1,8 @@
 package com.staffing.app.utils;
 
-import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
 import com.staffing.app.dao.EmployeeProjectRepository;
 import com.staffing.app.dao.EmployeeRepository;
 import com.staffing.app.dao.ProjectRepository;
+import com.staffing.app.dao.StaffingRepository;
 import com.staffing.app.model.Employee;
 import com.staffing.app.model.EmployeeProject;
 import com.staffing.app.model.Project;
+import com.staffing.app.model.Staffing;
 
 @Component
 public class AddData implements ApplicationListener<ContextRefreshedEvent> {
@@ -20,12 +23,15 @@ public class AddData implements ApplicationListener<ContextRefreshedEvent> {
 	EmployeeRepository employeeRepository;
 	EmployeeProjectRepository employeeProjectRepository;
 	ProjectRepository projectRepository;
+	StaffingRepository staffingRepository;
 
 	public AddData(EmployeeRepository employeeRepository, EmployeeProjectRepository employeeProjectRepository,
-			ProjectRepository projectRepository) {
+			ProjectRepository projectRepository, StaffingRepository staffingRepository) {
+		super();
 		this.employeeRepository = employeeRepository;
 		this.employeeProjectRepository = employeeProjectRepository;
 		this.projectRepository = projectRepository;
+		this.staffingRepository = staffingRepository;
 	}
 
 	@Override
@@ -44,13 +50,21 @@ public class AddData implements ApplicationListener<ContextRefreshedEvent> {
 		Project p = new Project();
 		p.setName("Victoria");
 		Project pSaved = projectRepository.save(p);
+		
+		
+		
+		
 		EmployeeProject empr = new EmployeeProject();
 		empr.setRefEmployee(empSaved);
 		empr.setRefProject(pSaved);
 		empr.setDateFrom(new Date());
 		empr.setDateTo(new Date());
-		employeeProjectRepository.save(empr);
-
+		
+		Staffing s = new Staffing();
+		s.setDateStaffing(new Date());
+		s.setDuration("8h");
+		s.setRefEmployeeProject(employeeProjectRepository.save(empr));
+		staffingRepository.save(s);
 	}
 
 }
